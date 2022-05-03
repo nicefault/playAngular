b9b84e036f7dad6452fabb195f8f74f040dda80b
taehoonhwang <on91716@gmail.com>
2022-05-03 23:44:29 +0900

    Udemy section 5-68

---
+++ cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [
+    {type:'server', name:'TestServer', content:'Just a test!'},
+    {type:'server', name:'TestServer', content:'Just a test!'}
+  ];
+
+  onServerAdded(){
+
+  }
+  onBlueprintAdded(){
+
+  }
+}
